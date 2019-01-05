9562b4e749b9f2d6b4de4e0fa9333f2e005a53b2
Ferenc Hechler <github@fh.anderemails.de>
2019-01-06 00:08:50 +0100

    shell and batch files

---
+++ executable/urlmonitor.bat
@@ -0,0 +1,1 @@
+java -jar urlmonitor.jar "https://raw.githubusercontent.com/ferenc-hechler/UrlMonitor/master/README.md" "urlmonitor.log" 15 15 "STATUS=200"
---
+++ executable/urlmonitor.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+java -jar urlmonitor.jar "https://raw.githubusercontent.com/ferenc-hechler/UrlMonitor/master/README.md" "urlmonitor.log" 15 15 "STATUS=200"
